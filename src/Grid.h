23cc9937cae17fbab8f4b74704938329f2f1be8f
Thomas Jakway <tjakway@gmail.com>
2015-08-30 17:41:41 -0400

    added a definition TESTING to segregate testing-related code added FRIEND_TEST macro to class Grid for testing of private members

--- src/Grid.h
+++ src/Grid.h
@@ -3,14 +3,25 @@
 
 #include <list>
 
+#ifdef TESTING
+#include "gtest/gtest.h"
+#endif
+
 /**
  * TODO: handle grid resizing and tile setting
  */
 class Grid
 {
+#ifdef TESTING
+    FRIEND_TEST(GridTests, testCtor);
+    FRIEND_TEST(GridTests, testExpandSize);
+#endif
+
 private:
     std::list<std::list<bool>*> tiles;
     /** expand the grid by adding a column to each side and a row to the top and bottom*/
+
+protected:
     void expandGrid();
     /** returns true if there are live tiles on the edge of the grid
      *  call this function at the beginning of every generation to guarantee the grid
--- test/GridTest.cpp
+++ test/GridTest.cpp
@@ -1,9 +1,16 @@
 #include "gtest/gtest.h"
 #include "Grid.h"
 
-TEST(grid_tests, test_ctor)
+TEST(GridTests, testCtor)
 {
     const int width=10, height=10;
     Grid grid(width, height);
     ASSERT_TRUE(true);
+}
+
+TEST(GridTests, testExpandSize)
+{
+    const int width=10, height=10;
+    Grid grid(width, height);
+    grid.expandGrid();
 }
