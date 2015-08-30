757030101bd25028079795442db491c591d507fc
Thomas Jakway <tjakway@gmail.com>
2015-08-30 17:19:44 -0400

    added basic CTOR test

--- test/GridTest.cpp
+++ test/GridTest.cpp
@@ -1,6 +1,9 @@
 #include "gtest/gtest.h"
+#include "Grid.h"
 
-TEST(grid_tests, testgrid)
+TEST(grid_tests, test_ctor)
 {
+    const int width=10, height=10;
+    Grid grid(width, height);
     ASSERT_TRUE(true);
 }
