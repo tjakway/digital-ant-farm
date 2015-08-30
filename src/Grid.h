cad6aa36cc995f992503133060caca04e555c286
Thomas Jakway <tjakway@gmail.com>
2015-08-30 17:42:06 -0400

    restored expandGrid() to private access

--- src/Grid.h
+++ src/Grid.h
@@ -20,8 +20,6 @@ class Grid
 private:
     std::list<std::list<bool>*> tiles;
     /** expand the grid by adding a column to each side and a row to the top and bottom*/
-
-protected:
     void expandGrid();
     /** returns true if there are live tiles on the edge of the grid
      *  call this function at the beginning of every generation to guarantee the grid
