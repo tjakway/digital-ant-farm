8b5777db084ec9063f6ff4f70dca70e0ae29e794
Thomas Jakway <tjakway@gmail.com>
2015-09-18 08:46:07 -0400

    created CSV importer test skeleton

---
+++ test/GridCSVImportTest.cpp
@@ -0,0 +1,20 @@
+#ifndef ANTF_GRIDCSVIMPORTTEST_H
+#define ANTF_GRIDCSVIMPORTTEST_H
+
+#include "gtest/gtest.h"
+#include "Grid.hpp"
+
+namespace jakway_antf
+{
+
+    /**
+     * Test creating the grid from a CSV spreadsheet 
+     */
+TEST(GridCSVImportTests, testOneEmptyCSV)
+{
+
+}
+
+}
+
+#endif
