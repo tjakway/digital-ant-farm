4af63321a477506c63ce9c1847ff9ba3f9117df9
Thomas Jakway <tjakway@gmail.com>
2015-09-18 11:14:03 -0400

    wrote a basic test using a CSV resource

--- test/GridCSVImportTest.cpp
+++ test/GridCSVImportTest.cpp
@@ -14,8 +14,25 @@ namespace jakway_antf
 TEST(GridCSVImportTests, testOneEmptyCSV)
 {
     std::shared_ptr<Grid> grid = Grid::readGridFromCSV(TestConstants::ONE_4X4_CSV);
+    int x = 0, y = 0;
 
 
+    //the spreadsheet has 1's diagonally, like this:
+    //1 0 0 0
+    //0 1 0 0
+    //0 0 1 0
+    //0 0 0 1
+    //except that instead of 0's it has blank cells
+    for(bool tile : *grid)
+    {
+        if(x == y)
+            ASSERT_TRUE(tile == TILE_ALIVE);
+        else
+            ASSERT_TRUE(tile == TILE_DEAD);
+
+        x++;
+        y++;
+    }
 }
 
 }
