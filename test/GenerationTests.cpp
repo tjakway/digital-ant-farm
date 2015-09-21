d01da9d9695453f6ca3e6dd569ca56e1ee8ff64f
Thomas Jakway <tjakway@gmail.com>
2015-09-21 18:56:26 -0400

    wrote a test of a static shape in Conways Game of Life

--- test/GenerationTests.cpp
+++ test/GenerationTests.cpp
@@ -1,5 +1,6 @@
 #include "gtest/gtest.h"
 #include "Grid.hpp"
+#include <algorithm>
 
 namespace jakway_antf
 {
@@ -21,14 +22,27 @@ TEST(GenerationTests, testSquare)
      * grid.runGeneration will expand the grid since touchingEdges() is true
      * also change the name of GameRunner.runGeneration because 2 classes with different methods with the same name is confusing
      */
-    auto checkGrid = [&grid]() { 
-        for(POS_TYPE x = 0; x < width + 2; x++)
+    auto checkGrid = [&grid]() {
+        //check the top and bottom edges are dead--we expanded the graph
+        for(POS_TYPE x = 0; x < width + 1; x++)
         {
-            for(POS_TYPE y = 0; y < height + 2; y++)
-            {
+            ASSERT_TRUE(grid.getTile(x, 0) == TILE_DEAD);
+            ASSERT_TRUE(grid.getTile(x, height + 1) == TILE_DEAD);
+        }
 
-            }
-        }};
+        //check the left and right edges are dead
+        for(POS_TYPE y = 0; y < height + 1; y++)
+        {
+            ASSERT_TRUE(grid.getTile(y, 0) == TILE_DEAD);
+            ASSERT_TRUE(grid.getTile(y, width + 1) == TILE_DEAD);
+        }
+
+        //the middle of the graph is the square--it should be alive
+        ASSERT_TRUE(grid.getTile(1, 1) == TILE_ALIVE);
+        ASSERT_TRUE(grid.getTile(2, 1) == TILE_ALIVE);
+        ASSERT_TRUE(grid.getTile(1, 2) == TILE_ALIVE);
+        ASSERT_TRUE(grid.getTile(2, 2) == TILE_ALIVE);
+        };
 
     //set all tiles to alive
     std::for_each(grid.begin(), grid.end(), [](bool& tile) { tile = TILE_ALIVE; });
@@ -37,17 +51,16 @@ TEST(GenerationTests, testSquare)
     std::for_each(grid.begin(), grid.end(), [](bool& tile) { ASSERT_TRUE(tile == TILE_ALIVE); });
     ASSERT_TRUE(grid.getSize() == (width * height));
 
+    //after running the first generation the grid will be expanded--this requires a new checking function
     //run generations and assert nothing has changed
     const unsigned int numGenerations = 100;
     for(unsigned int i = 0; i < numGenerations ; i++)
     {
         //after each generation, check the size is still correct and all the tiles are the same
         grid.runGeneration();
-        ASSERT_TRUE(grid.getSize() == (width * height));
-        std::for_each(grid.begin(), grid.end(), [](bool& tile) { ASSERT_TRUE(tile == TILE_ALIVE); });
+        checkGrid();
+        ASSERT_TRUE(grid.getSize() == ((width+2) * (height+2)));
     }
-    
-    
 }
 
 }
