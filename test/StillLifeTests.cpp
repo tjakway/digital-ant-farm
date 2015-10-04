396ff3fdb00b3b326bad1d37e668820f907d9768
Thomas Jakway <tjakway@gmail.com>
2015-10-04 13:53:19 -0400

    wrote a test of the beehive still life

--- test/StillLifeTests.cpp
+++ test/StillLifeTests.cpp
@@ -49,3 +49,40 @@ TEST(StillLifeTests, testBlock)
         ASSERT_TRUE(grid.getSize() == ((width+2) * (height+2)));
     }
 }
+
+/**
+ * a non-square still life somewhat larger than the rectangle
+ */
+TEST(StillLifeTests, testBeehive)
+{
+    std::vector<std::array<POS_TYPE, 2>> liveTiles ={
+        {{1, 2}},
+        {{2, 1}},
+        {{2, 3}},
+        {{3, 1}},
+        {{3, 3}},
+        {{4, 2}} };
+
+    //initialize the grid with the list of live tiles
+    const POS_TYPE width=6, height=5;
+    Grid grid(width, height, liveTiles);
+
+    ASSERT_TRUE(grid.getSize() == (width * height));
+
+    auto checkGrid = [&grid, &liveTiles]() {
+
+        assertDeadExcept(&grid, liveTiles);
+        };
+
+    //run generations and assert nothing has changed
+    //the grid will NOT expand because it's been allocated with a 1-tile buffer
+    const unsigned int numGenerations = 100;
+    for(unsigned int i = 0; i < numGenerations ; i++)
+    {
+        //after each generation, check the size is still correct and all the tiles are the same
+        grid.runGeneration();
+        checkGrid();
+        //size should not have changed
+        ASSERT_TRUE(grid.getSize() == ((width) * (height)));
+    }
+}
