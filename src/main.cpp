0a9d8a24aa238da9960ad66c03a02334ed194792
Thomas Jakway <tjakway@gmail.com>
2015-10-09 18:50:43 -0400

    added some lines to the demo, nothing displayed yet

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
+#include <memory>
 
 #include "NativeWindow.hpp"
 #include "SDLWindow.hpp"
+#include "Grid.hpp"
 using namespace jakway_antf;
 
 int main(int argc, char** argv)
 {
-    std::cout << "Hello, world!" << std::endl;
     NativeWindow* win = new SDLWindow();
+
+    std::shared_ptr<Grid> ptr = std::shared_ptr<Grid>(new Grid(2, 2));
+    ptr->setTile(0, 0, TILE_ALIVE);
+    ptr->setTile(1, 1, TILE_ALIVE);
+
+    win->postIO(ptr);
     win->beginEventLoop(argc, argv);
 }
