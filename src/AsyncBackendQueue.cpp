fa39dfcc5e3feeeacbe1e83482dfaf9b8ea45650
Thomas Jakway <tjakway@gmail.com>
2015-10-10 15:49:20 -0400

    AsyncBackendQueue now loops infinitely to keep checking the queue added explanation of purpose

--- src/AsyncBackendQueue.cpp
+++ src/AsyncBackendQueue.cpp
@@ -6,19 +6,24 @@ void AsyncBackendQueue::backendThreadProc(AsyncBackendQueue *asyncBackendQueue)
 {
     std::unique_lock<std::mutex> lock(asyncBackendQueue->queueMutex);
 
-    //sleep until we have something to draw
-    while(asyncBackendQueue->genDrawList.empty())
-    {
-        asyncBackendQueue->queueCond.wait(lock);
-    }
+    //wait until we have something in the queue
+    asyncBackendQueue->queueCond.wait(lock, [asyncBackendQueue](){ return !asyncBackendQueue->genDrawList.empty(); });
 
     //draw events in the order they're added to the queue
     std::shared_ptr<Grid> oldestEvent = asyncBackendQueue->genDrawList.back();
     //delete the item we just fetched
     asyncBackendQueue->genDrawList.pop_back(); 
     
-
     asyncBackendQueue->processIO(oldestEvent);
+
+    //unlock (to prevent deadlock) and recurse so the thread loops forever
+    //and keeps checking for events
+    //it ought to:
+    //-wait until it finds an event
+    //-call processIO and pass that event
+    //-start waiting again
+    lock.unlock();
+    backendThreadProc(asyncBackendQueue);
 }
 
 AsyncBackendQueue::AsyncBackendQueue()
